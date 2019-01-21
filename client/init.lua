06e8724c5ddfd833238d1fcf921073e145c0190c
APTXOUS <2392100311@qq.com>
2019-01-21 15:12:12 +0800

    from aptxtion

--- client/init.lua
+++ client/init.lua
@@ -3,22 +3,20 @@
 --the firmware need end user setup module
 print('Setting up WIFI...')
 wifi.setmode(wifi.STATIONAP)
-wifi.ap.config({ssid="nodemcu", pwd="12345678", auth=wifi.WPA2_PSK})
+wifi.ap.config({ssid="NODEMCU", pwd="12345678", auth=wifi.WPA2_PSK})
 
-
-enduser_setup.manual(true)
 print("ap ip:"..wifi.ap.getip())
 print("ap mac:"..wifi.ap.getmac())
 print("sta mac:"..wifi.sta.getmac())
 
 
+enduser_setup.manual(true)  
 enduser_setup.start(
-    function()
-      print("sta ip:" .. wifi.sta.getip())
-      wifi.setmode(wifi.STATION)
-    end,
-    function(err, str)
-      print("enduser_err:" .. str)
-      enduser_stop()
-    end
-  )
+  function()
+    print("Connectedto   wifi ip is:" .. wifi.sta.getip())   
+  end,
+  function(err, str)
+    print("enduser_setup: Err #" .. err .. ": " .. str)
+  end
+)
+
