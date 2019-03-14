71d5a3208d0c49ea61878efa4c838b81e3ccab2e
APTXOUS-MAC <2392100311@qq.com>
2019-03-14 16:45:15 +0800

    server changed

--- server/main.cpp
+++ server/main.cpp
@@ -1,12 +1,35 @@
 //socket udp 服务端
+//包名
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <string>
 
 const int BUFF_LEN = 200;
+
+
+struct package{
+    char Id[16];    //15位设备ID
+    char Divide;    //是否分包 0：不分包 其他：分包的标示
+    char Seq;       //第几个包
+    char Len;       //包长度
+    char Type;      //包类型
+    char Info[100]; //属性
+};
+
+
+void save_machince_info()
+{
+
+}
+
+void save_status()
+{
+
+}
 
 int main()
 {
@@ -26,6 +49,17 @@ int main()
         printf("bind\n");
         return -1;
     }
+
+    //---------------------初始化完成------------------------------
+    //任务流程
+    //1:监听udp包
+    //2:分析udp包
+    //  回复ack包
+    //  若是设备id是初次连接，保存设备id
+    //  若设备id已经被保存，更新设备连接时间
+    
+    
+    
     struct sockaddr_in cli;
     socklen_t len = sizeof(cli);
     int count;
@@ -39,10 +73,10 @@ int main()
             return 0;
         }
         printf("server recv: %s\n", buf);
-        printf("recv from: %s:%d",inet_ntoa(cli.sin_addr),cli.sin_port);
+        printf("recv from: %s:%d\n",inet_ntoa(cli.sin_addr),cli.sin_port);
         memset(buf, 0, BUFF_LEN);
-        sprintf(buf, "I have recieved %d bytes data!\n", count);           //回复client
-        printf("server send: %s\n", buf);                                        //打印自己发送的信息给
+        sprintf(buf, "I have recieved %d bytes data!\n", count);                //回复client
+        printf("server send: %s\n", buf);                                       //打印自己发送的信息给
         sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&cli, len);
     }
     close(sockfd);
