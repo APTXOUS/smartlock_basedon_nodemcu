e0c7d10d23e846e56bbbd60b9bcb18fa8489173e
APTXOUS <2392100311@qq.com>
2019-03-20 14:57:51 +0800

    test

--- WebSite/php/login_process.php
+++ WebSite/php/login_process.php
@@ -7,10 +7,10 @@
     $userName = addslashes($_POST['userName']);
     $password = addslashes($_POST['password']);
 
-    $mysqli=getConnect();
+    //$mysqli=getConnect();
     $loginSQL = "select count(*) from security where uid= '$userName' AND password='$password'";
     echo $loginSQL; 
-    $result = $mysqli->query($loginSQL);
-    echo $result->num_rows;
+    //$result = $mysqli->query($loginSQL);
+    //echo $result->num_rows;
     //closeConnect();
 ?>
